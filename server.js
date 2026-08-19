584204d13470a148e8fed7683796053f6731c85a
jwfst5088 <jwfst5088@outlook.com>
2026-08-19 04:06:11 +0000

    修复端口：改为8080

--- server.js
+++ server.js
@@ -583,7 +583,7 @@ app.get('/favicon.ico',(req,res)=>{
   res.status(204).end();
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 async function startup() {
   db = initDB();
   server.listen(PORT, '0.0.0.0', () => {
